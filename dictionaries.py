cf6928ea75195b6b57229b971bd3e8e4874f7d19
Garvitjoshi1 <garvit.12106692@lpu.in>
2024-01-02 23:39:22 +0530

    Dictionaries
    Signed-off-by: Garvitjoshi1 <garvit.12106692@lpu.in>

--- dictionaries.py
+++ dictionaries.py
@@ -1,1 +1,10 @@
 # A dictionary is a mutable data type that stores mappings of unique keys to values
+elements = {"hydrogen": 1, "helium": 2, "carbon": 6, "lithium": 3}
+print(elements)
+print("hydrogen" in elements)
+print(elements.get("oxygen"))  # to extract the key.
+
+# Identity Operators:- is, is not
+x = elements.get("oxygen")
+not_null = x is not None
+print(not_null)
