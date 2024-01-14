73ef5cab9baf1f235de581e7e33df89f03d51172
Garvitjoshi1 <garvit.12106692@lpu.in>
2024-01-14 16:41:03 +0530

    Organising Modules
    Signed-off-by: Garvitjoshi1 <garvit.12106692@lpu.in>

--- Organising into modules/Gaussiandistribution.py
+++ Organising into modules/Gaussiandistribution.py
@@ -2,4 +2,9 @@ import math
 import matplotlib.pyplot as plt
 from Generaldistriubtion import Distribution
 
+
 class Gaussian(Distribution):
+    """
+    Import properties from GeneralDistribution file.
+    this is how OOPs is used.
+    """
