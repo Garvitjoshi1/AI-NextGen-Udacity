ea178fe1c32db94462c86373e7190562168545ab
Garvitjoshi1 <garvit.12106692@lpu.in>
2024-01-15 08:35:05 +0530

    Python Packages
    Signed-off-by: Garvitjoshi1 <garvit.12106692@lpu.in>

---
+++ Python_Package_Example/Organising into modules/Gaussiandistribution.py
@@ -0,0 +1,10 @@
+import math
+import matplotlib.pyplot as plt
+from .Generaldistriubtion import Distribution
+
+
+class Gaussian(Distribution):
+    """
+    Import properties from GeneralDistribution file.
+    This is how OOP is used.
+    """
